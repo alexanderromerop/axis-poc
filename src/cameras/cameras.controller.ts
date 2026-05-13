99193737b5d0ae635a3f8a1dca3c514fcf5a963b
aromerop <aromerop@elecnor.es>
2026-05-13 13:29:10 +0200

    feat: MQTT event added

--- src/cameras/cameras.controller.ts
+++ src/cameras/cameras.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body, Logger, HttpCode, Get, Req } from '@nestjs/common';
 import { CamerasService } from './cameras.service';
+import { Ctx, MessagePattern, MqttContext, Payload } from '@nestjs/microservices';
 
 @Controller('cameras')
 export class CamerasController {
@@ -11,15 +12,20 @@ export class CamerasController {
     return this.camerasService.testRoute();
   }
 
-  @Post('events/human-detection')
-  @HttpCode(200)
-  humanDetection(@Req() request: Request, @Body() body: any) {
-    return this.camerasService.humanDetection(request, body);
-  }
+  // @Post('events/human-detection')
+  // @HttpCode(200)
+  // humanDetection(@Req() request: Request, @Body() body: any) {
+  //   return this.camerasService.humanDetection(request, body);
+  // }
 
-  @Post('events/line-crossing')
-  @HttpCode(200)
-  lineCrossing(@Req() request: Request, @Body() body: any) {
-    return this.camerasService.lineCrossing(request, body);
+  // @Post('events/line-crossing')
+  // @HttpCode(200)
+  // lineCrossing(@Req() request: Request, @Body() body: any) {
+  //   return this.camerasService.lineCrossing(request, body);
+  // }
+
+  @MessagePattern('camera/VMD')
+  handleMQTTEvents(@Payload() data: any, @Ctx() context: MqttContext) {
+    return this.camerasService.handleAxisEvents(data, context);
   }
 }
--- src/cameras/cameras.service.ts
+++ src/cameras/cameras.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Payload, Ctx, MqttContext } from '@nestjs/microservices';
 
 @Injectable()
 export class CamerasService {
@@ -9,15 +10,28 @@ export class CamerasService {
 
   humanDetection(request: Request, body: any) {
     console.log('\n--- MOVEMENT DETECTION ---');
-    console.log('Content-Type:', request.headers['content-type']);
     console.log('Body:', body);
     console.log('-----------------------------------------\n');
   }
 
   lineCrossing(request: Request, body: any) {
     console.log('\n--- LINE CROSSING ---');
-    console.log('Content-Type:', request.headers['content-type']);
     console.log('Body:', body);
     console.log('-----------------------------------------\n');
   }
+
+    handleAxisEvents(data: any, context: MqttContext) {
+      const topic = context.getTopic();
+      
+      console.log(`\n--- MQTT Event ---`);
+      console.log(`Topic: ${topic}`);
+      
+      try {
+        const parsedData = typeof data === 'string' ? JSON.parse(data) : data;
+        console.log('Payload:', parsedData);
+        
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    }
 }
