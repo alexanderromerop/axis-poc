38e3374e915adb7f84e8de243cc64a93e1745794
aromerop <aromerop@elecnor.es>
2026-05-15 13:12:43 +0200

    feat: snapshot axis done

--- src/snapshots/snapshots.service.ts
+++ src/snapshots/snapshots.service.ts
@@ -1,21 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import AxiosDigestAuth from '@mhoc/axios-digest-auth';
-import * as fs from 'fs';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import * as https from 'https';
 import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class SnapshotsService implements OnModuleInit {
   private readonly logger = new Logger(SnapshotsService.name);
   private readonly snapshotDir = path.join(process.cwd(), "snapshot");
-  private digestAuth: AxiosDigestAuth;
 
-  constructor(private configService: ConfigService) {
-    this.digestAuth = new AxiosDigestAuth({
-      username: this.configService.get<string>('CAMERA_USER') || '',
-      password: this.configService.get<string>('CAMERA_PASSWORD') || ''
-    });
-  }
+  constructor(
+    private readonly httpsService: HttpService,
+    private configService: ConfigService
+  ) {}
 
   onModuleInit() {
     this.isSnapshotDir(this.snapshotDir);
@@ -40,27 +39,37 @@ export class SnapshotsService implements OnModuleInit {
 
   async getLastSnapshot(cameraManufacturer: string) {
     const cameraIp = this.configService.get<string>('CAMERA_IP');
+    const username = this.configService.get<string>('CAMERA_USER') || '';
+    const password = this.configService.get<string>('CAMERA_PASSWORD') || '';
 
     let url;
     switch (cameraManufacturer) {
       case "Hanwha":
-        url = `http://${cameraIp}/stw-cgi/video.cgi?msubmenu=snapshot&action=view`;
+        url = `https://${cameraIp}/stw-cgi/video.cgi?msubmenu=snapshot&action=view`;
         break;
 
       case "Axis":
-        url = `http://${cameraIp}/axis-cgi/jpg/image.cgi`
+        url = `https://${cameraIp}/axis-cgi/jpg/image.cgi`
         break;
     }
+
+    const agent = new https.Agent({
+      rejectUnauthorized: false,
+    });
 
     try {
       this.logger.log(`Getting snapshot from ${cameraIp}...`);
 
-      const response = await this.digestAuth.request({
-        method: 'GET',
-        url: url,
-        responseType: 'arraybuffer',
-        timeout: 5000,
-      });
+      const response = await firstValueFrom(
+        this.httpsService.get(url, {
+          httpsAgent: agent,
+          auth: {
+            username: username,
+            password: password, 
+          },
+          responseType: 'arraybuffer',
+        }),
+      );
 
       const fileName = `snapshot_${Date.now()}.jpg`;
       const completeRoute = path.join(this.snapshotDir, fileName);
