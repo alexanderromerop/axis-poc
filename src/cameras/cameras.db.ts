a96451ee7fb503e0629cba1f88f64fc71d96a2f7
aromerop <aromerop@elecnor.es>
2026-05-26 10:14:16 +0200

    feat: axis ip change

--- src/cameras/cameras.db.ts
+++ src/cameras/cameras.db.ts
@@ -3,14 +3,14 @@ import { ICamera } from "./interface/camera.interface";
 export const camerasDb: ICamera[] = [
   {
     manufacturer: "Axis",
-    ip: "192.168.88.254",
+    ip: "192.168.88.253",
     user: "admin",
     password: "@Elecnor1!",
     id: "e89b",
   },
   {
     manufacturer: "Hanwha",
-    ip: "192.168.88.253",
+    ip: "192.168.88.254",
     user: "admin",
     password: "@Elecnor1",
     id: "a456",
